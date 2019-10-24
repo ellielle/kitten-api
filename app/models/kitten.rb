c816440c810a3fa2d209c24ac628666da317467c
ellielle <40385743+ellielle@users.noreply.github.com>
2019-10-24 10:48:32 -0400

    add validations

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,6 @@
 class Kitten < ApplicationRecord
+  validates :age, presence: true, numericality: true, inclusion: { in: 0..20 }
+  validates :name, presence: true
+  validates :softness, presence: true, inclusion: { in: softness }
+  validates :cuteness, presence: true, inclusion: { in: cuteness }
 end
