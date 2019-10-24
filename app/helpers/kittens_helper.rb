96d1e49024182bb767f52b8ee2267c80959eaf83
ellielle <40385743+ellielle@users.noreply.github.com>
2019-10-24 10:14:19 -0400

    add helpers for collections on forms

--- app/helpers/kittens_helper.rb
+++ app/helpers/kittens_helper.rb
@@ -1,2 +1,9 @@
 module KittensHelper
+  def cuteness
+    %w(Adorable Lovely Magnificent Incredibly Very Kawaii)
+  end
+
+  def softness
+    ["As a feather", "Super Floof", "Fleece Blanket", "Hairless", "Purrfect"]
+  end
 end
