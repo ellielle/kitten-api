950faf3648116aa1067c80b3af213910b4b03157
ellielle <40385743+ellielle@users.noreply.github.com>
2019-10-24 13:22:45 -0400

    refactor methods to not have naming issues

--- app/helpers/kittens_helper.rb
+++ app/helpers/kittens_helper.rb
@@ -1,9 +1,9 @@
 module KittensHelper
-  def cuteness
+  def cute_list
     %w(Adorable Lovely Magnificent Incredibly Very Kawaii)
   end
 
-  def softness
+  def soft_list
     ["As a feather", "Super Floof", "Fleece Blanket", "Hairless", "Purrfect"]
   end
 end
